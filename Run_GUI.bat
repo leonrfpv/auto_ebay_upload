f80cc27409317a1f710c76df7b4a38e72132004b
leonrfpv <leonrfpv@gmail.com>
2025-08-30 20:35:45 +0200

    Update Run_GUI.bat

--- Run_GUI.bat
+++ Run_GUI.bat
@@ -1,1 +1,13 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+
+if not exist .venv (
+  echo Bitte zuerst Install_Or_Update.bat ausfuehren.
+  pause
+  exit /b 1
+)
+
+call .venv\Scripts\activate
+call .venv\Scripts\python.exe app\auto_ebay_upload.py
 
